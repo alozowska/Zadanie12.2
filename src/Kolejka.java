c90619cfffcbc0df1c2ee5b02e1e7edca0b1e6be
Agnieszka Łozowska <aga.lozowska@wp.pl>
2018-08-13 16:53:15 +0200

    poprawione liczenie

--- src/Kolejka.java
+++ src/Kolejka.java
@@ -5,34 +5,33 @@ import java.util.Scanner;
 
 public class Kolejka {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("podaj liczbę");
-        int liczba=scanner.nextInt();
-        Queue<Integer>liczby=new LinkedList<>();
-
+        Scanner scanner = new Scanner(System.in);
+        // System.out.println("podaj liczbę");
+        int liczba;
+        Queue<Integer> liczby = new LinkedList<>();
         for (int i = 0; i < 10; i++) {
-            liczby.offer(liczba);
             System.out.println("podaj liczbę");
-           liczba=scanner.nextInt();
+            liczba = scanner.nextInt();
+            liczby.offer(liczba);
         }
-
         sumaLiczb(liczby);
     }
-   public static void sumaLiczb( Queue<Integer>liczby){
-       int suma=0;
-       int i=0;
-           Iterator<Integer> iterator = liczby.iterator();
-           while (iterator.hasNext()) {
-               Integer next = iterator.next();
-               System.out.print(next);
-               i++;
-               if(i<liczby.size()) {
-                   System.out.print("+");
-               }
-               suma=suma+next;
-               }
-       System.out.println("="+ suma);
-           }
+
+    public static void sumaLiczb(Queue<Integer> liczby) {
+        int suma = 0;
+        int i = 0;
+        Iterator<Integer> iterator = liczby.iterator();
+        while (iterator.hasNext()) {
+            Integer next = iterator.next();
+            System.out.print(next);
+            i++;
+            if (i < liczby.size()) {
+                System.out.print("+");
+            }
+            suma = suma + next;
+        }
+        System.out.println("=" + suma);
     }
+}
 
 
